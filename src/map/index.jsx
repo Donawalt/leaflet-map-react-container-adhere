ece55eb4cd6cf8127885d914fb181e591c91d699
Donaël Walt <w.dodo.a@hotmail.fr>
2020-02-13 13:44:12 +0100

    create base props

--- src/map/index.jsx
+++ src/map/index.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import L from "leaflet";
 
-function Map(height, width) {
+function Map(props) {
+  console.log(props);
   useEffect(() => {
     var circles = [];
     var maker = [];
@@ -10,36 +11,11 @@ function Map(height, width) {
     var nivZoom = 13; // initial zoom level
     var currentView = [48.8620543, 2.3449645];
 
-    var eventLocation = [
-      {
-        name: "Gare de Lyon",
-        location: [48.8443038, 2.3743773],
-        stations: 0
-      },
-      {
-        name: "Chatelet les Halles",
-        location: [48.8620543, 2.3449645],
-        stations: 0
-      },
-      {
-        name: "Nation ",
-        location: [48.8483634, 2.393789],
-        stations: [
-          {
-            name: "rue des boulet",
-            location: [48.8522705, 2.3872059]
-          },
-          {
-            name: "picpus",
-            location: [48.8449438, 2.3990587]
-          }
-        ]
-      }
-    ]; //dummy data in the goal to try  the principle
-
+    var eventLocation = props.eventLocationData.dataEventLocation; //dummy data in the goal to try  the principle
+    console.log(eventLocation);
     var Icon = L.icon({
       // Création d'icone personalisée
-      iconUrl: "./epreuve.png", // Créer une variable suivant une condition
+      iconUrl: props.iconUrl.iconUrlChallenge, // Créer une variable suivant une condition
 
       iconSize: [50, 63], // size of the icon
       iconAnchor: [25, 63], // point of the icon which will correspond to marker's location
@@ -48,14 +24,16 @@ function Map(height, width) {
 
     var stationIcon = L.icon({
       // Création d'icone personalisée
-      iconUrl: "./station.png", // Créer une variable suivant une condition
+      iconUrl: props.iconUrl.iconUrlStation, // Créer une variable suivant une condition
 
       iconSize: [30, 34], // size of the icon
       iconAnchor: [25, 34], // point of the icon which will correspond to marker's location
       popupAnchor: [-10, -34] // point from which the popup should open relative to the iconAnchor
     }); // We initialise the icon
 
-    var mymap = L.map("map_id").setView(currentView, nivZoom); // we initialize the map
+    var mymap = L.map("map_id",{
+      scrollWheelZoom: false
+    }).setView(currentView, nivZoom); // we initialize the map
 
     const init = () => {
       L.tileLayer(
@@ -175,7 +153,8 @@ function Map(height, width) {
 
     return main(); // Run the main  function
   }, []);
-  return <div id="map_id" className="map" style={height} />;
+
+  return <div id="map_id" className="map" style={{height: props.height}} />;
 }
 
 export default Map;
